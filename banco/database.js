26c1b56f23075571e7dc54854651101ad351b46c
jonatajap <89617950+jonatajap@users.noreply.github.com>
2023-06-02 13:46:47 -0300

    knex e pg conectados

---
+++ banco/database.js
@@ -0,0 +1,10 @@
+var knex = require('knex')({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'root',
+      password : '',
+      database : 'bdtiresolve'
+    }
+  });
